ce963fb6c1db7f1aa4f2473a212b5729f0b25dad
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2019-06-28 10:18:22 +0200

    add failing test

--- game-of-life/rust/game-of-life/src/main.rs
+++ game-of-life/rust/game-of-life/src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
     println!("Hello, world!");
 }
+#[derive(Debug, PartialEq)]
+enum CellType {
+    Dead,
+    Alive
+}
+
+type Grid = Vec<Vec<CellType>>;
+
+fn evolve(seed: &Grid) -> Grid {
+    return Vec::new();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::CellType::*;
+
+    #[test]
+    fn a_block_evolves_into_a_block() {
+        let seed = vec![
+            vec![Dead, Dead, Dead, Dead],
+            vec![Dead, Alive, Alive, Dead],
+            vec![Dead, Alive, Alive, Dead],
+            vec![Dead, Dead, Dead, Dead],
+        ];
+
+        let new_generation = evolve(&seed);
+
+        assert_eq!(new_generation, seed);
+    }
+}

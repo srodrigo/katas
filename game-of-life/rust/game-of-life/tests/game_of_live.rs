41caf55f2a7429fbde5c9b589911c05851a181aa
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2019-06-30 14:31:41 +0200

    refactor: use files for the remaining tests

--- game-of-life/rust/game-of-life/tests/game_of_live.rs
+++ game-of-life/rust/game-of-life/tests/game_of_live.rs
@@ -8,103 +8,42 @@ mod tests {
 
     #[test]
     fn a_block_evolves_into_a_block() {
-        let seed = load_grid("block");
-
-        let new_generation = evolve(&seed);
-
-        assert_eq!(new_generation, load_grid("block"));
+        assert_eq!(
+            evolve(&load_grid("block")),
+            load_grid("block")
+        );
     }
 
     #[test]
     fn a_vertical_oscillator_evolves_into_a_horizonal_one() {
-        let seed = vec![
-            vec![Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Alive, Dead, Dead],
-            vec![Dead, Dead, Alive, Dead, Dead],
-            vec![Dead, Dead, Alive, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead],
-        ];
-
-        let new_generation = evolve(&seed);
-
-        let horizontal_oscillator = vec![
-            vec![Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Alive, Alive, Alive, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead],
-        ];
-        assert_eq!(new_generation, horizontal_oscillator);
+        assert_eq!(
+            evolve(&load_grid("vertical-oscillator")),
+            load_grid("horizontal-oscillator")
+        );
     }
 
     #[test]
     fn a_horizontal_oscillator_evolves_into_a_vertical_one() {
-        let horizontal_oscillator = vec![
-            vec![Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Alive, Alive, Alive, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead],
-        ];
-
-        let new_generation = evolve(&horizontal_oscillator);
-
-        let vertical_oscillator = vec![
-            vec![Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Alive, Dead, Dead],
-            vec![Dead, Dead, Alive, Dead, Dead],
-            vec![Dead, Dead, Alive, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead],
-        ];
-        assert_eq!(new_generation, vertical_oscillator);
+        assert_eq!(
+            evolve(&load_grid("horizontal-oscillator")),
+            load_grid("vertical-oscillator")
+        );
     }
 
     #[test]
     fn a_packed_toad_evolves_into_an_open_one() {
-        let pack_toad = vec![
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Alive, Alive, Alive, Dead],
-            vec![Dead, Alive, Alive, Alive, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-        ];
-
-        let new_generation = evolve(&pack_toad);
-
-        let open_toad = vec![
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Alive, Dead, Dead],
-            vec![Dead, Alive, Dead, Dead, Alive, Dead],
-            vec![Dead, Alive, Dead, Dead, Alive, Dead],
-            vec![Dead, Dead, Alive, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-        ];
-        assert_eq!(new_generation, open_toad);
+        assert_eq!(
+            evolve(&load_grid("packed-toad")),
+            load_grid("open-toad")
+        );
     }
 
     #[test]
     fn an_open_toad_evolves_into_a_packed_one() {
-        let open_toad = vec![
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Alive, Dead, Dead],
-            vec![Dead, Alive, Dead, Dead, Alive, Dead],
-            vec![Dead, Alive, Dead, Dead, Alive, Dead],
-            vec![Dead, Dead, Alive, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-        ];
-
-        let new_generation = evolve(&open_toad);
-
-        let packed_toad = vec![
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Alive, Alive, Alive, Dead],
-            vec![Dead, Alive, Alive, Alive, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-            vec![Dead, Dead, Dead, Dead, Dead, Dead],
-        ];
-        assert_eq!(new_generation, packed_toad);
+        assert_eq!(
+            evolve(&load_grid("open-toad")),
+            load_grid("packed-toad")
+        );
     }
 
     fn load_grid(filename: &str) -> Grid {

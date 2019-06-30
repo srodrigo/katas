a614eaf7c5703ec7ad9656e8f1bebbd8416c3d3b
Sergio Rodrigo Royo <sergio.rodrigo.royo@gmail.com>
2019-06-30 13:33:27 +0200

    add a few more GoL tests

--- game-of-life/rust/game-of-life/src/main.rs
+++ game-of-life/rust/game-of-life/src/main.rs
@@ -131,4 +131,74 @@ mod tests {
         ];
         assert_eq!(new_generation, horizontal_oscillator);
     }
+
+    #[test]
+    fn a_horizontal_oscillator_evolves_into_a_vertical_one() {
+        let horizontal_oscillator = vec![
+            vec![Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Alive, Alive, Alive, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead],
+        ];
+
+        let new_generation = evolve(&horizontal_oscillator);
+
+        let vertical_oscillator = vec![
+            vec![Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Alive, Dead, Dead],
+            vec![Dead, Dead, Alive, Dead, Dead],
+            vec![Dead, Dead, Alive, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead],
+        ];
+        assert_eq!(new_generation, vertical_oscillator);
+    }
+
+    #[test]
+    fn a_packed_toad_evolves_into_an_open_one() {
+        let pack_toad = vec![
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Alive, Alive, Alive, Dead],
+            vec![Dead, Alive, Alive, Alive, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+        ];
+
+        let new_generation = evolve(&pack_toad);
+
+        let open_toad = vec![
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Alive, Dead, Dead],
+            vec![Dead, Alive, Dead, Dead, Alive, Dead],
+            vec![Dead, Alive, Dead, Dead, Alive, Dead],
+            vec![Dead, Dead, Alive, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+        ];
+        assert_eq!(new_generation, open_toad);
+    }
+
+    #[test]
+    fn an_open_toad_evolves_into_a_packed_one() {
+        let open_toad = vec![
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Alive, Dead, Dead],
+            vec![Dead, Alive, Dead, Dead, Alive, Dead],
+            vec![Dead, Alive, Dead, Dead, Alive, Dead],
+            vec![Dead, Dead, Alive, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+        ];
+
+        let new_generation = evolve(&open_toad);
+
+        let packed_toad = vec![
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Alive, Alive, Alive, Dead],
+            vec![Dead, Alive, Alive, Alive, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+            vec![Dead, Dead, Dead, Dead, Dead, Dead],
+        ];
+        assert_eq!(new_generation, packed_toad);
+    }
 }
